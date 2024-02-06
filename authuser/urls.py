45a2e4115077cecdb35ead3d73ea9e0e4672059d
Vu Hong Tuyen <tuyenvu524@gmail.com>
2024-02-06 09:07:18 +0700

    change password type 1

--- authuser/urls.py
+++ authuser/urls.py
@@ -3,4 +3,6 @@ from authuser import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('changepassword', views.changepassword, name='changepassword'),
+    path('upgrade', views.upgrade, name='upgrade'),
 ]
--- authuser/views.py
+++ authuser/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render,get_object_or_404,redirect
 # Create your views here.
 def index(request):
     return render(request, 'profile.html')
+
+def changepassword(request):
+    return render(request, 'changepassword.html')
+
+def upgrade(request):
+    return render(request, 'upgrade.html')
