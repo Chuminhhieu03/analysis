9e00dd26c55fb347e78db587c5a175d720343e69
Nammmmmmmm <NamPP.B21CN555@stu.ptit.edu.vn>
2024-02-07 15:19:29 +0700

    firir

--- authuser/urls.py
+++ authuser/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from authuser import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='profile_user'),
+    path('/edit', views.edit_profile, name='edit_profile_user'),
+    path('/feedback', views.feedback_user, name='feedback_user')
 ]
--- authuser/views.py
+++ authuser/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render,get_object_or_404,redirect
 # Create your views here.
 def index(request):
     return render(request, 'profile.html')
+
+def edit_profile(request):
+    return render(request, "editprofile.html")
+
+def feedback_user(request):
+    return render(request, "feedback.html")
