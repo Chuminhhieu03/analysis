ff1fc7fc6db56fc26787c761e8f44d512558395c
Dat Ta Tien <ta.dat123@gmail.com>
2024-02-07 15:36:12 +0700

    Djtm3chuhieu

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,9 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('income/', views.income, name='income'),
+    path('expense/', views.expense, name='expense'),
 ]
+
+
+
