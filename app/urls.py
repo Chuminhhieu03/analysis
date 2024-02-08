bd67c470885926e87fd8d582c69ef603e5ac61dc
chuhieu03 <chuhieuqb2003@gmail.com>
2024-02-08 07:59:16 +0700

    merge Datbranch

--- app/urls.py
+++ app/urls.py
@@ -3,8 +3,6 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('income/', views.income, name='income'),
-    path('expense/', views.expense, name='expense'),
 ]
 
 
---
+++ income/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from income import views
+
+urlpatterns = [
+    path('', views.index, name='income'),
+]
+
+
+
